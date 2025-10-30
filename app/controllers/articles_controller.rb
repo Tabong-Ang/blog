162385d38fb94ec8853e0c128b4a3f712c0541d4
Tabong-Ang <tabongphilip@gmail.com>
2025-10-30 10:40:48 +0100

    implemented CRUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,43 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @articles = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def article_params
+      params.expect(article: [ :title ])
+    end
 end
