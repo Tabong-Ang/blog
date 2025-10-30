ea5b42fb46d735c4285acdeb0063daa6bf6d58e2
Tabong-Ang <tabongphilip@gmail.com>
2025-10-30 11:38:55 +0100

    I will still need to learn the whole process again as from here

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  get "/signup", to: "users#new", as: :signup
   root "articles#index"
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
