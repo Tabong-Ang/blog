3f2f70fb9b00a2dbb15a9f40c0a25c59e5ac56a5
Tabong-Ang <tabongphilip@gmail.com>
2025-11-20 19:59:32 +0100

    Added corresponding #create action to build a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,5 +9,16 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = current_user.posts.build(user_params)
+    if @post.save
+      redirect_to posts_path, notice: "Your blog was created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def user_params
+      params.expect(post: [:title, :description, :image_url])
+    end
 end
